9a909faeb8d7d62b3e6e6adf98f5e09a9dc41d2d
fadooda <fadyibrahim37@gmail.com>
2020-05-03 23:13:18 -0400

    Different query solution, that returns minimum id along with the respective unique emails without deleting it from the original table

--- SQL/Delete Duplicate Emails.sql
+++ SQL/Delete Duplicate Emails.sql
@@ -1,3 +1,10 @@
 /*Delete Duplicate Emails*/
 delete prow from person prow, person p2table 
-where prow.email = p2table.email and prow.id > p2table.id; /* it will delete duplicate rows based on, the same email and the greater id (to keep the minimum id)*/
+where prow.email = p2table.email and prow.id > p2table.id; /* it will delete duplicate rows based on: the same email and the greater id (to keep the minimum id)*/
+
+
+/* Different query solution, that returns minimum id along with the respective unique emails without deleting it from the original table*/
+select min(id) as Id, Email
+from Person
+group by email
+order by id; 
