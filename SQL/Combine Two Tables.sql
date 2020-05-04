ec63915be692af47d1bbd1517a89309c73e15806
fadooda <fadyibrahim37@gmail.com>
2020-05-03 20:17:46 -0400

    added semicolon, fixed compliation issue

--- SQL/Combine Two Tables.sql
+++ SQL/Combine Two Tables.sql
@@ -2,6 +2,6 @@
 select p.FirstName, p.LastName, a.City, a.State
 from person p 
 left join Address a /*left join to include person(s) that do not have a registered address (address will be displayed as null)*/
-on p.personid =a.personid
+on p.personid =a.personid;
 
 
