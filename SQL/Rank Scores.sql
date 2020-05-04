e925fbec95382afc20ac23467cff5d4d36f92814
fadooda <fadyibrahim37@gmail.com>
2020-05-03 22:27:29 -0400

    updated query to place together the sccore with the repective rank

--- SQL/Rank Scores.sql
+++ SQL/Rank Scores.sql
@@ -1,2 +1,7 @@
-Select Count(1) + 1 
-From (Select Distinct Score from Scores) as rank
+/* Rank scores*/ 
+Select sboard.Score,
+       (Select Count(1) + 1  /* add one to the counter to begin at 1 instead of zero  */
+	   From (Select Distinct Score from Scores) as uniqScore 
+	   where sboard.Score <uniqScore.Score) as "Rank"  /*to only increment when the sboard is less than uniqScore*/
+From Scores sboard 
+Order by sboard.Score Desc;
