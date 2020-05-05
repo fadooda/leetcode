b5b022e7dddb10bb1e7b18bbd53a0f2f85814d36
fadooda <fadyibrahim37@gmail.com>
2020-05-05 14:27:52 -0400

    fixed a comment

--- SQL/Rank Scores.sql
+++ SQL/Rank Scores.sql
@@ -2,6 +2,6 @@
 Select sboard.Score,
        (Select Count(1) + 1  /* add one to the counter to begin at 1 instead of zero  */
 	   From (Select Distinct Score from Scores) as uniqScore 
-	   where sboard.Score <uniqScore.Score) as "Rank"  /*to only increment when the sboard is less than uniqScore*/
+	   where sboard.Score <uniqScore.Score) as "Rank"  /*only increment when the sboard is less than uniqScore*/
 From Scores sboard 
 Order by sboard.Score Desc;
