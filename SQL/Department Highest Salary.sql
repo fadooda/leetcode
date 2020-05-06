409cf409c6f4eee2cf111ef7b3e30f94853892e4
fadooda <fadyibrahim37@gmail.com>
2020-05-06 14:18:03 -0400

    added outter query to select employees with the highest salary in their department

--- SQL/Department Highest Salary.sql
+++ SQL/Department Highest Salary.sql
@@ -1,5 +1,7 @@
-/*Write your MySQL query statement below*/
-select max(e.salary) from Employee e
-inner join Department d
-on e.DepartmentId = d.id
-group by e.DepartmentId
+/* Write your MySQL query statement below*/
+select dep.Name as "Department", emp.Name as "Employee", emp.Salary
+from Employee emp
+inner join Department dep
+on emp.DepartmentId = dep.id
+where emp.Salary in (select max(e.salary) from Employee e where e.DepartmentId =  dep.id group by e.DepartmentId); /*subquery gets the max salary for each department id group */
+
