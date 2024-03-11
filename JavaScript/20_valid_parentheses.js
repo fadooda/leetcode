9720843f7981a878c78996e1690952902d4ba276
Fady Ibrahim <fadyibrahim37@gmail.com>
2024-03-11 15:07:21 -0400

    removed overhead from function and swtiched to use map to store unqui key->value pair

--- JavaScript/20_valid_parentheses.js
+++ JavaScript/20_valid_parentheses.js
@@ -2,32 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-function charMap(char){
-    if(char==='{')
-    {
-        return '}'
-    }else if (char==='[')
-    {
-        return ']'
-    }else if (char==='(')
-    {
-        return ')'
-    }
-}
-
-
-
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
     let stack=[]
+    let parmap= new Map([
+  ['{', '}'],
+  ['[', ']'],
+  ['(', ')']
+]);
     for(let index=0; index < s.length;index++){
         let char=s.charAt(index)
         let peepchar=stack[stack.length-1]
-        let mappedchar=charMap(peepchar)
-        if(char!=mappedchar){
+        if(char!=parmap.get(peepchar)){
            stack.push(char)
         }else {
             stack.pop()
@@ -35,4 +20,3 @@ var isValid = function(s) {
     }
     return stack.length==0
 };
-    
