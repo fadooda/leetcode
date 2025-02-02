0191bcb4a117e7cac7746df6b0ebadd351c0caf7
Fady Ibrahim <fadyibrahim37@gmail.com>
2025-02-01 22:12:05 -0500

    optimized solution. Used E6 syntax to make it more readable and reduce lines of code

--- JavaScript/CounterII.js
+++ JavaScript/CounterII.js
@@ -1,26 +1,14 @@
 /**
  * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
+ * @return {Object}
  */
 var createCounter = function(init) {
     let currentValue = init;
-    return { 
-        increment: function() {
-           return  ++currentValue;
-        }, 
-        decrement: function() {
-           return --currentValue;
-        }, 
-        reset: function() {
-         currentValue = init ;
-         return currentValue
-        }
-    }
+
+    return {
+        increment: () => ++currentValue,
+        decrement: () => --currentValue,
+        reset: () => (currentValue = init)
+    };
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
